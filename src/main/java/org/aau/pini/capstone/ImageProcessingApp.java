6bdab923b746e3a869ffbc421361852afb850aa4
ITPini <marc6571us@gmail.com>
2023-01-19 23:24:45 +0100

    Window is now maximized

--- src/main/java/org/aau/pini/capstone/ImageProcessingApp.java
+++ src/main/java/org/aau/pini/capstone/ImageProcessingApp.java
@@ -1,7 +1,3 @@
-/**
- * @author Marcelino Patrick Pini - mpini21@student.aau.dk
- */
-
 package org.aau.pini.capstone;
 
 import javafx.application.Application;
@@ -9,12 +5,16 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * @author Marcelino Patrick Pini - mpini21@student.aau.dk
+ */
 public class ImageProcessingApp extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(ImageProcessingApp.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Image Processing");
+        stage.setMaximized(true);
         stage.setScene(scene);
         stage.show();
     }
